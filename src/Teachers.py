70424a043442c4d2e290461b2326934f6ceaafe6
Abimbola <Haryishar2@gmail.com>
2025-03-06 00:46:16 +0100

    tasks

--- src/Teachers.py
+++ src/Teachers.py
@@ -6,19 +6,19 @@ class Teachers:
 
     def __init__(self):
         self.teacher_list = []
-    def add_teacher(self, teacher_name):
-        if teacher_name not in self.teacher_list:
-            self.teacher_list.append(teacher_name)
-            print(f"Teacher {teacher_name} added.")
-        else:
-            print(f"Teacher {teacher_name} is already in the list.")
 
-    def remove_teacher(self, teacher_name):
-        if teacher_name in self.teacher_list:
-            self.teacher_list.remove(teacher_name)
-            return f"Teacher {teacher_name} removed."
-        else:
-            raise NotFoundException(f"{teacher_name} is not in the list.")
+    def add_teacher(self, teacher_name):
+        teacher = {"name": teacher_name}
+        self.teacher_list.append(teacher)
+        return teacher
+
+    def remove_teacher(self, teacher_name: str) -> bool:
+        for teacher in self.teacher_list:
+            if teacher["name"] == teacher_name:
+                self.teacher_list.remove(teacher)
+                return True
+        raise NotFoundException(f"{teacher_name} is not in the list.")
+
 
     def view_teachers(self):
         return self.teacher_list
--- tests/test_teachers.py
+++ tests/test_teachers.py
@@ -12,3 +12,12 @@ class MyTeachersTestCase(unittest.TestCase):
         name = self.teachers_management.add_teacher("Dr Ayo")
         self.assertIn(name, self.teachers_management.view_teachers())
 
+    def test_that_a_teacher_can_be_removed(self):
+        name = self.teachers_management.add_teacher("Dr Ayo")
+        second_name = self.teachers_management.add_teacher("Dr Favour")
+        self.assertIn(name, self.teachers_management.view_teachers())
+        self.assertIn(second_name, self.teachers_management.view_teachers())
+        self.teachers_management.remove_teacher("Dr Favour")
+        self.assertNotIn(second_name, self.teachers_management.view_teachers())
+        self.assertTrue(second_name not in self.teachers_management.view_teachers())
+
